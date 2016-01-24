3cdb02a026fd2186ccc4154571078e2fbab9ba74
Nic Jackson <jackson.nic@gmail.com>
2016-01-24 19:26:54 +0000

    updated to use new build gem

--- _build/features/support/env.rb
+++ _build/features/support/env.rb
@@ -3,9 +3,9 @@ require_relative 'cucumber_rest_monkey_patch'
 require 'cucumber/pickle_mongodb'
 require 'cucumber/mailcatcher'
 
-$SERVER_PATH = ENV['WEB_SERVER_URI']
-$REDIS_IP = ENV['REDIS_IP']
-$REDIS_PORT = ENV['REDIS_PORT']
-$MIMIC_SERVER = ENV['MIMIC_SERVER']
+$SERVER_PATH = "http://#{ENV['DOCKER_IP']}:8001"
+$REDIS_IP = ENV['DOCKER_IP']
+$REDIS_PORT = 6379
+$MIMIC_SERVER = "http://#{ENV['DOCKER_IP']}:11988"
 
 Mongoid.load!('features/support/localdb.yml', :development)
